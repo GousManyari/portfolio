70a532c4f05b9ad8143e105d74db4ddee4db9083
Gous Manyari <gousmanyari@gmail.com>
2020-06-12 20:27:13 +0530

    Updates

---
+++ precache-manifest.ea680ff1ad202074179f63f8e786c0cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "56e8e6164f1e28bfa0af",
+    "url": "/portfolio/static/css/main.93eed301.chunk.css"
+  },
+  {
+    "revision": "56e8e6164f1e28bfa0af",
+    "url": "/portfolio/static/js/main.165adc72.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "255367876796b9da7e92",
+    "url": "/portfolio/static/css/2.9ec2dbc9.chunk.css"
+  },
+  {
+    "revision": "255367876796b9da7e92",
+    "url": "/portfolio/static/js/2.ea178ca4.chunk.js"
+  },
+  {
+    "revision": "badc931e4999cc54c3b2ffc62646c12e",
+    "url": "/portfolio/static/media/male.badc931e.png"
+  },
+  {
+    "revision": "2b926ff5fa0a69c51d25b25ada0b98ce",
+    "url": "/portfolio/index.html"
+  }
+];
