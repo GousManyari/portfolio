b00707ed8a28f9beba77d841fa52419b022c353b
Gous Manyari <gousmanyari@gmail.com>
2020-06-15 02:22:45 +0530

    about page created

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { Grid, Cell,List,ListItem,ListItemContent} from "react-mdl";
+import male from '../asset/img/male.png';
 
 class About extends Component {
     render(){
         return (
-            <div><h1>About Page</h1></div>
+          
+            <div className="about-body">
+                <Grid className="about-grid" >
+
+                    <Cell col={6} >
+                        <h3>Gous Manyari</h3>
+                        <img src={male} alt="Gous Manyari"
+                            style={{width:'250px', }}
+                        />
+                        <p>Hello Dummy Text Apparently we had reached a great height in the atmosphere,
+                            for the sky was a dead black, and the stars had ceased to twinkle.
+                            By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside,
+                        </p>
+                    </Cell>
+
+                    <Cell col={6}>
+                        <h3>Contact Me</h3>
+                        <hr/>
+                        <List style={{marginTop:'3em',justifyContent:'center'}}>
+                            <ListItem style={{fontFamily:'Audiowide',fontSize:'25px'}}>
+                                <ListItemContent icon="phone_android"  className="avatar">+91-7755952294</ListItemContent>
+                            </ListItem>
+                            <ListItem style={{fontFamily:'Audiowide',fontSize:'25px'}}>
+                                <ListItemContent icon="person">Gous Manyari</ListItemContent>
+                            </ListItem>
+                            <ListItem style={{fontFamily:'Audiowide',fontSize:'25px'}}>
+                                <ListItemContent icon="mail">gousmanyari@gmail.com</ListItemContent>
+                            </ListItem>
+                        </List>
+                    </Cell>
+
+                </Grid>
+            </div>
         )
     }
 }
